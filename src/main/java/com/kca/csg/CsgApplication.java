d387880c62666fe95d73f9b2d9fbeba6dd4c9921
codepakr-kr <codepark.kr@gmail.com>
2021-11-29 00:22:38 +0900

    [ADD] Add Welcome-page, test log-code also
    - Finish to Setting up the Development Environment
    - Add test-log in Main Application file
    - Add welcome-page in resources/static

--- src/main/java/com/kca/csg/CsgApplication.java
+++ src/main/java/com/kca/csg/CsgApplication.java
@@ -1,13 +1,19 @@
 package com.kca.csg;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CsgApplication {
 
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(CsgApplication.class);
+		logger.info("working well");
 		SpringApplication.run(CsgApplication.class, args);
 	}
-
 }
