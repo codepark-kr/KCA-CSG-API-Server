53b6f981a532a5fb91d474a456597204f4c51229
codepakr-kr <codepark.kr@gmail.com>
2021-11-28 21:37:05 +0900

    [CFG] Initialize Spring Boot

---
+++ src/main/java/com/kca/csg/CsgApplication.java
@@ -0,0 +1,13 @@
+package com.kca.csg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kca/csg/CsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kca.csg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
