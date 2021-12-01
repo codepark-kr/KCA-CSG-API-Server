7dbdb37bd00ac04310a9f71636cae2e3be4f4338
박예빈 <codepark.kr@gmail.com>
2021-12-01 11:28:45 +0900

    [ADD] Add demo models, dto, controller
    - Add DemoModel, DTOs, Controller, Entity
      for testing to req/res with rest-api structure

---
+++ src/main/java/com/kca/csg/DTO/TodoDTO.java
@@ -0,0 +1,24 @@
+package com.kca.csg.DTO;
+
+import com.kca.csg.model.TodoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TodoDTO {
+    private String id;
+    private String title;
+    private boolean done;
+
+//  TODO @codepark: userId will be used for authentication with spring security.
+    public TodoDTO(final TodoEntity entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.done = entity.isDone();
+    }
+}
