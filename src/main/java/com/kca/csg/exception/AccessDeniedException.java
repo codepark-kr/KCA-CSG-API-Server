9b8916ae221aa555957e116bc33c173e8c6761d3
박예빈 <codepark.kr@gmail.com>
2021-12-16 11:33:24 +0900

    [ADD] add custom-exception classes

---
+++ src/main/java/com/kca/csg/exception/AccessDeniedException.java
@@ -0,0 +1,43 @@
+package com.kca.csg.exception;
+
+import com.kca.csg.payload.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+public class AccessDeniedException  extends RuntimeException{
+
+    private ApiResponse apiResponse;
+
+    private String message;
+
+    public AccessDeniedException(ApiResponse apiResponse){
+        super();
+        this.apiResponse = apiResponse;
+    }
+
+    public AccessDeniedException(String message){
+        super(message);
+        this.message = message;
+    }
+
+    public AccessDeniedException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public ApiResponse getApiResponse(){
+        return apiResponse;
+    }
+
+    public void setApiResponse(ApiResponse apiResponse){
+        this.apiResponse = apiResponse;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
