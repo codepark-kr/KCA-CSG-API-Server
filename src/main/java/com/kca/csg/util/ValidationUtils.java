acff4ee4b6f4b2793e8562923a3454cf7a2f043c
박예빈 <codepark.kr@gmail.com>
2021-12-16 18:42:23 +0900

    [REF] rename the AppUtils -> ValidationUtils
    - As what already wrote on TODO@TodoServiceImpl,
      the same or similar conditional-statement and
      declaration of returning custom-exceptions are
      repeating to many times, so surely it needs to
      refactoring
    - So first, change the ambiguous name of AppUtil
      to ValidationUtils
    - Then, will add statements for validations
      to ValidationUtil

--- src/main/java/com/kca/csg/util/ValidationUtils.java
+++ src/main/java/com/kca/csg/util/ValidationUtils.java
@@ -3,13 +3,12 @@ package com.kca.csg.util;
 import com.kca.csg.exception.ApiException;
 import org.springframework.http.HttpStatus;
 
-public class AppUtils {
+public class ValidationUtils {
     public static void validatePageNumberAndSize(int page, int size){
         if(page < 0){ throw new ApiException(HttpStatus.BAD_REQUEST, "Page number cannot be less than zero"); }
         if(size < 0){ throw new ApiException(HttpStatus.BAD_REQUEST, "Size number cannot be less than zero"); }
-
         if(size > AppConstants.MAX_PAGE_SIZE){
-            throw new ApiException(HttpStatus.BAD_REQUEST, "Page size must not be grater than " + AppConstants.MAX_PAGE_SIZE);
+            throw new ApiException(HttpStatus.BAD_REQUEST, "Page size must not be greater than " + AppConstants.MAX_PAGE_SIZE);
         }
     }
 }
