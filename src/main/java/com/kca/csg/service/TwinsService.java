bd89799602b2875619d944b44cc23ee7a42ece8a
박예빈 <codepark.kr@gmail.com>
2021-12-16 17:30:20 +0900

    [STYLE] Add or remove line-breaks

--- src/main/java/com/kca/csg/service/TwinsService.java
+++ src/main/java/com/kca/csg/service/TwinsService.java
@@ -10,18 +10,14 @@ import com.kca.csg.security.UserPrincipal;
 public interface TwinsService {
 
     PagedResponse<Twins> getAllTwins(int page, int size);
-
     PagedResponse<Twins> getTwinsByCreatedBy(String username, int page, int size);
-
     PagedResponse<Twins> getTwinsByCategory(Long id, int page, int size);
-
     PagedResponse<Twins> getTwinsByTag(Long id, int page, int size);
 
     Twins updateTwins(Long id, TwinsRequest newTwinsRequest, UserPrincipal currentUser);
+    Twins getTwins(Long id);
 
     ApiResponse deleteTwins(Long id, UserPrincipal currentUser);
-
     TwinsResponse addTwins(TwinsRequest twinsRequest, UserPrincipal currentUser);
 
-    Twins getTwins(Long id);
 }
--- src/main/java/com/kca/csg/service/UserService.java
+++ src/main/java/com/kca/csg/service/UserService.java
@@ -12,19 +12,15 @@ public interface UserService {
     UserSummary getCurrentUser(UserPrincipal currentUser);
 
     UserIdentityAvailability checkUsernameAvailability(String username);
-
     UserIdentityAvailability checkEmailAvailability(String email);
 
     UserProfile getUserProfile(String username);
 
     User addUser(User user);
-
     User updateUser(User newUser, String username, UserPrincipal currentUser);
 
     ApiResponse deleteUser(String username, UserPrincipal currentUser);
-
     ApiResponse grantAdmin(String username);
-
     ApiResponse removeAdmin(String username);
 
 }
