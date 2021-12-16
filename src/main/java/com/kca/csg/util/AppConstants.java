a0afbb25850543c2e171f4414e1744f81c863481
박예빈 <codepark.kr@gmail.com>
2021-12-16 14:05:42 +0900

    [ADD] Add common Utils like Constrants, AppUtils

---
+++ src/main/java/com/kca/csg/util/AppConstants.java
@@ -0,0 +1,18 @@
+package com.kca.csg.util;
+
+public class AppConstants {
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "30";
+    public static final int MAX_PAGE_SIZE = 30;
+    public static final String CREATED_AT = "createdAt";
+    public static final String ID = "id";
+    public static final String PHOTO = "Photo";
+    public static final String ALBUM = "Album";
+    public static final String USERNAME = "username";
+    public static final String CATEGORY = "Category";
+    public static final String TAG = "Tag";
+    public static final String TWINS = "Twins";
+    public static final String TODO = "Todo";
+    public static final String NO_PERMISSION = "You dont' have permission to make this operation";
+}
