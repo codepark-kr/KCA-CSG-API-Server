4c7cdc85d7f6e62b1d45639b64b56418c8a4dfc4
박예빈 <codepark.kr@gmail.com>
2021-12-17 14:16:24 +0900

    [CFG] Add configuration to user SonarQube
    - Add configuration for SonarQube to build.gradle
    - Add @WebAppConfiguration to testApplication file

--- src/test/java/com/kca/csg/CsgApplicationTests.java
+++ src/test/java/com/kca/csg/CsgApplicationTests.java
@@ -2,7 +2,9 @@ package com.kca.csg;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @SpringBootTest
 class CsgApplicationTests {
 
