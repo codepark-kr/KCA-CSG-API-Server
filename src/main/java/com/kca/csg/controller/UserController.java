44d2ba03fffc0d075d72d09b24bebb757ba0dcec
박예빈 <codepark.kr@gmail.com>
2021-12-17 10:46:08 +0900

    [REF] Refactor the way of Dependency Injection
    - Refactor the way of dependency injection
      from Field-injection to Constructor-injection

--- src/main/java/com/kca/csg/controller/UserController.java
+++ src/main/java/com/kca/csg/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.kca.csg.controller;
 
 
-import com.kca.csg.payload.UserIdentityAvailability;
 import com.kca.csg.payload.UserSummary;
 import com.kca.csg.security.CurrentUser;
 import com.kca.csg.security.UserPrincipal;
 import com.kca.csg.service.AlbumService;
 import com.kca.csg.service.TwinsService;
 import com.kca.csg.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,15 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+    private final TwinsService twinsService;
+    private final AlbumService albumService;
 
-    @Autowired
-    private TwinsService twinsService;
-
-    @Autowired
-    private AlbumService albumService;
-
+    public UserController(UserService userService, TwinsService twinsService, AlbumService albumService) {
+        this.userService = userService;
+        this.twinsService = twinsService;
+        this.albumService = albumService;
+    }
 
     @GetMapping("/me")
     @PreAuthorize("hasRole('USER')")
