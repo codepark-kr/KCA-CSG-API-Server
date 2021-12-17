c384a5125c4f51d0cbcc664053c4dfa3898a1747
박예빈 <codepark.kr@gmail.com>
2021-12-17 13:09:50 +0900

    [ADD] Add new Util the find global resource by id
    - Add new util for find global resource by specific id
    - Change the commonUtils name to globalUtil
      (Guess It's more explicit)

---
+++ src/main/java/com/kca/csg/util/GlobalUtils.java
@@ -0,0 +1,42 @@
+package com.kca.csg.util;
+
+import com.kca.csg.exception.ResourceNotFoundException;
+import com.kca.csg.repository.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import static com.kca.csg.util.AppConstants.*;
+
+public class GlobalUtils {
+
+    private static TodoRepository todoRepository;
+    private static CategoryRepository categoryRepository;
+    private static TagRepository tagRepository;
+    private static TwinsRepository twinsRepository;
+    private static UserRepository userRepository;
+
+    public GlobalUtils(TodoRepository todoRepository, CategoryRepository categoryRepository, TagRepository tagRepository,
+                       TwinsRepository twinsRepository, UserRepository userRepository) {
+        GlobalUtils.todoRepository = todoRepository;
+        GlobalUtils.categoryRepository = categoryRepository;
+        GlobalUtils.tagRepository = tagRepository;
+        GlobalUtils.twinsRepository = twinsRepository;
+        GlobalUtils.userRepository = userRepository;
+    }
+
+    public static Pageable sortDescending(int page, int size){
+        return PageRequest.of(page, size, Sort.Direction.DESC, CREATED_AT);
+    }
+
+    public static Object findResourceById(Long id, String resourceName, Object fieldValue){
+        switch(resourceName){
+            case TODO: return todoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(TODO, ID, fieldValue));
+            case TWINS: return twinsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(TWINS, ID, fieldValue));
+            case CATEGORY: return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(CATEGORY, ID, fieldValue));
+            case TAG: return tagRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(TAG, ID, fieldValue));
+            case USER: return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(USER, ID, fieldValue));
+            default: return null;
+        }
+    }
+}
