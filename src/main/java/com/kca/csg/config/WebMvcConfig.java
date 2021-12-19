d647a72f76512bdfa9167d3f4d870f35c0e71940
codepakr-kr <codepark.kr@gmail.com>
2021-12-19 23:07:52 +0900

    [FIX] Remove useless @Value annotation

--- src/main/java/com/kca/csg/config/WebMvcConfig.java
+++ src/main/java/com/kca/csg/config/WebMvcConfig.java
@@ -1,14 +1,11 @@
 package com.kca.csg.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Value("cors.allowedOrigins")
     public void addCorsMappings(CorsRegistry registry){
         final long MAX_AGE_SECS = 3600;
 
