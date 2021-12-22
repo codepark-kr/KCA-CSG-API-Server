9c458eb4849ca6361122a11b4e097324f1d8ec11
박예빈 <codepark.kr@gmail.com>
2021-12-22 11:11:40 +0900

    [DEBUG] Fix the wrong type of dependency injection
    - The wrong type of JwtTokenProvider and Custom-
      UserDetailsService returned null
    - So when try to get profile via api/user/me,
      NullPointerException occurred
    - Fixed that issue with @Autowired

--- src/main/java/com/kca/csg/security/JwtAuthenticationFilter.java
+++ src/main/java/com/kca/csg/security/JwtAuthenticationFilter.java
@@ -1,14 +1,13 @@
 package com.kca.csg.security;
 
 import com.kca.csg.service.CustomUserDetailsService;
-import lombok.NoArgsConstructor;
 import org.mariadb.jdbc.internal.logging.Logger;
 import org.mariadb.jdbc.internal.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,18 +17,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
-@NoArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
+    @Autowired
     private JwtTokenProvider tokenProvider;
-    private CustomUserDetailsService customUserDetailsService;
 
-    public JwtAuthenticationFilter(JwtTokenProvider tokenProvider, CustomUserDetailsService customUserDetailsService) {
-        this.tokenProvider = tokenProvider;
-        this.customUserDetailsService = customUserDetailsService;
-    }
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -39,16 +34,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             if(StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)){
                 Long userId = tokenProvider.getUserIdFromJWT(jwt);
-
                 UserDetails userDetails = customUserDetailsService.loadUserById(userId);
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken
                         (userDetails, null, userDetails.getAuthorities());
+
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken); }
         }catch(Exception e){
             LOGGER.error("Could not set user authentication in security context", e);
-        }
-        filterChain.doFilter(request, response);
+        } filterChain.doFilter(request, response);
     }
     private String getJwtFromRequest(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
