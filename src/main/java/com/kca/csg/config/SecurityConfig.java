503b58c831b0381d2c3b6eea5e1ad5bad0c75315
박예빈 <codepark.kr@gmail.com>
2021-12-23 10:36:29 +0900

    [ADD] Add h2, swagger to auth_whitelist and apply
    - Remove seperated method for h2-ignore configuration
    - Add AUTH_WHITELIST as String[] static final variable
      And applied it to antMatchers, it works well

--- src/main/java/com/kca/csg/config/SecurityConfig.java
+++ src/main/java/com/kca/csg/config/SecurityConfig.java
@@ -33,29 +33,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
+    private static final String[] AUTH_WHITELIST = {
+            "/h2-console/**",
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/webjars/**",
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .antMatchers(HttpMethod.GET, "/api/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/user/checkUsername", "/api/user/checkEmail").permitAll()
                 .anyRequest().authenticated();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    public void passwordConfigure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
         authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
-    public void h2Configure(WebSecurity webSecurity){ webSecurity.ignoring().antMatchers("/h2-console/**"); }
-
+//    @Override
+//    public void configure(WebSecurity webSecurity){
+//        webSecurity.ignoring().antMatchers("/h2-console/**", "/swagger-ui/index.html/**");
+//    }
 
     @Bean(BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManagerBean() throws Exception{
