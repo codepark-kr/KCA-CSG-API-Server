06bc300b95ba7635c0be2c8f40caad8c928efde9
박예빈 <codepark.kr@gmail.com>
2021-12-23 10:34:42 +0900

    [DEBUG] Fix the wrong-method name that cause error

--- src/main/java/com/kca/csg/config/SwaggerConfig.java
+++ src/main/java/com/kca/csg/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public Docket swaggerConfig(){
+    public Docket SwaggerConfig(){
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.kca.csg")).paths(PathSelectors.any()).build();
     }
