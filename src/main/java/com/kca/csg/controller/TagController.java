e52a966295aeffea48548706e7bf35641ce4d2e1
박예빈 <codepark.kr@gmail.com>
2021-12-23 18:28:34 +0900

    [WIP] Work in progress : TagController

---
+++ src/main/java/com/kca/csg/controller/TagController.java
@@ -0,0 +1,10 @@
+package com.kca.csg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tags")
+public class TagController {
+
+}
