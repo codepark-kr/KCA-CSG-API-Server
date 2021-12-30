79d62b293a8cd3bc52e821441b3b006d44515f1f
codepakr-kr <codepark.kr@gmail.com>
2021-12-30 22:46:25 +0900

    [ADD] Add new demo-contents for Recent Short Reports

--- src/main/frontend/src/pages/HorizontalWrapper.js
+++ src/main/frontend/src/pages/HorizontalWrapper.js
@@ -11,10 +11,122 @@ const HorizontalWrapper=()=>{
                     <div className="bg1">
                         < Header />
                         < TestComponent />
+                        <div className="shortReports">
+                            <div className="pearlTitle">Recent Short Reports</div>
+                            <table>
+                                <tr>
+                                    <td className="shortReportsTitle">
+                                        <span>Lorem Ipsum Neque Est.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsContents">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem recusandae tenetur, voluptates?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem.
+                                    </td>
+                                </tr>
+                                <tr className="tags">
+                                    <td className="tags"><span>#Agile</span> <span>#Development Methodology</span> <span>#TDD</span></td>
+                                </tr>
+                                <tr>
+                                    <td className="dates">2021.12.30.</td>
+                                </tr>
+
+                                <tr>
+                                    <td className="shortReportsTitle">
+                                        <span>Lorem Ipsum Neque Est.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsContents">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem recusandae tenetur, voluptates?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="tags"><span>#Agile</span> <span>#Development Methodology</span> <span>#TDD</span></td>
+                                </tr>
+                                <tr>
+                                    <td className="dates">2021.12.30.</td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsTitle">
+                                        <span>Lorem Ipsum Neque Est.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsContents">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem recusandae tenetur, voluptates?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="tags"><span>#Agile</span> <span>#Development Methodology</span> <span>#TDD</span></td>
+                                </tr>
+                                <tr>
+                                    <td className="dates">2021.12.30.</td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsTitle">
+                                        <span>Lorem Ipsum Neque Est.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsContents">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem recusandae tenetur, voluptates?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="tags"><span>#Agile</span> <span>#Development Methodology</span> <span>#TDD</span></td>
+                                </tr>
+                                <tr>
+                                    <td className="dates">2021.12.30.</td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsTitle">
+                                        <span>Lorem Ipsum Neque Est.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsContents">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem recusandae tenetur, voluptates?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="tags"><span>#Agile</span> <span>#Development Methodology</span> <span>#TDD</span></td>
+                                </tr>
+                                <tr>
+                                    <td className="dates">2021.12.30.</td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsTitle">
+                                        <span>Lorem Ipsum Neque Est.</span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="shortReportsContents">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem recusandae tenetur, voluptates?
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nihil nostrum odio temporibus tenetur? Commodi deserunt esse excepturi illo iure iusto modi nam non nulla qui quidem.
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="tags"><span>#Agile</span> <span>#Development Methodology</span> <span>#TDD</span></td>
+                                </tr>
+                                <tr>
+                                    <td className="dates">2021.12.30.</td>
+                                </tr>
+
+
+                            </table>
+                        </div>
                     </div>
                 </div>
                 <div className="item">
-                    <div className="bg2" ></div>
+                    <div className="bg2" >
+
+                    </div>
                 </div>
                 <div className="item">
                     <div className="bg"></div>
